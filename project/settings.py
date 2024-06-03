c8929a1abb0cee8c10a877013f7fff7106fb0944
Enthony <enthonyruan05@gmail.com>
2024-06-03 16:20:08 -0300

    terminei

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-*5y(6ugkj)hb()tc^mud1-57mwddxdwddm5106l$v%q5hdu&li
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
